73f6f99ddb5daa3cf9df7fab069ed852ea5693d2
ysl2007 <ysl.2007@gmail.com>
2015-09-22 09:59:23 +0800

    9.22 add all documents

--- src/edu/bit/eline/demo/ImgPanel.java
+++ src/edu/bit/eline/demo/ImgPanel.java
@@ -5,53 +5,53 @@ import java.awt.Image;
 import javax.swing.JPanel;
 
 public class ImgPanel extends JPanel {
-	private static final long serialVersionUID = 1L;
-	private Image image;
-	private int imgWidth;
-	private int imgHeight;
+    private static final long serialVersionUID = 1L;
+    private Image             image;
+    private int               imgWidth;
+    private int               imgHeight;
 
-	public Image getImg() {
-		return image;
-	}
+    public Image getImg() {
+        return image;
+    }
 
-	public int getImgWidth() {
-		return imgWidth;
-	}
+    public int getImgWidth() {
+        return imgWidth;
+    }
 
-	public void setImgWidth(int imgWidth) {
-		this.imgWidth = imgWidth;
-	}
+    public void setImgWidth(int imgWidth) {
+        this.imgWidth = imgWidth;
+    }
 
-	public int getImgHeight() {
-		return imgHeight;
-	}
+    public int getImgHeight() {
+        return imgHeight;
+    }
 
-	public void setImgHeight(int imgHeight) {
-		this.imgHeight = imgHeight;
-	}
+    public void setImgHeight(int imgHeight) {
+        this.imgHeight = imgHeight;
+    }
 
-	public ImgPanel() {}
+    public ImgPanel() {
+    }
 
-	public void setImage(Image img) {
-		if (null != img) {
-			image = img;
-			setImgWidth(image.getWidth(this));
-			setImgHeight(image.getHeight(this));
-		}
-	}
+    public void setImage(Image img) {
+        if (null != img) {
+            image = img;
+            setImgWidth(image.getWidth(this));
+            setImgHeight(image.getHeight(this));
+        }
+    }
 
-	@Override
-	public void paintComponent(Graphics g1) {
-		super.paintComponent(g1);
-		int x = 0;
-		int y = 0;
-		Graphics g = (Graphics) g1;
-		if (null == image) {
-			return;
-		}
+    @Override
+    public void paintComponent(Graphics g1) {
+        super.paintComponent(g1);
+        int x = 0;
+        int y = 0;
+        Graphics g = (Graphics) g1;
+        if (null == image) {
+            return;
+        }
 
-		g.drawImage(image, x, y, image.getWidth(this), image.getHeight(this),
-				this);
-		g = null;
-	}
+        g.drawImage(image, x, y, image.getWidth(this), image.getHeight(this), this);
+        g = null;
+    }
 }
