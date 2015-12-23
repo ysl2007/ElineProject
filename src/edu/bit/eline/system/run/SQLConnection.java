3054b9812a97fea9fdbf772b5ad6036081afdd06
ysl2007 <ysl.2007@gmail.com>
2015-12-23 09:55:47 +0800

    12/23, 单类别跑通

--- src/edu/bit/eline/system/run/SQLConnection.java
+++ src/edu/bit/eline/system/run/SQLConnection.java
@@ -7,7 +7,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.swing.JOptionPane;
 
@@ -16,111 +15,83 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class SQLConnection {
-<<<<<<< HEAD
     private String     configFile = "./config.json";
     private String     dbURL;
     private String     userName;
     private String     passwd;
     private Connection dbConn;
-=======
-	private String configFile = "./config.json";
-	private String dbURL;
-	private String userName;
-	private String passwd;
-	private Connection dbConn;
->>>>>>> 684225cc7bb26d1ee97e79544279a38be5c74d5c
 
-	public SQLConnection() {
-		JSONTokener tokener;
-		try {
-			tokener = new JSONTokener(new FileReader(configFile));
-		} catch (FileNotFoundException e) {
-			JOptionPane.showMessageDialog(null, "找不到配置文件。", "错误",
-					JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
-			return;
-		}
-		JSONObject jo = new JSONObject(tokener);
-		try {
-			dbURL = jo.getString("database_url");
-			userName = jo.getString("database_username");
-			passwd = jo.getString("database_password");
-		} catch (JSONException e) {
-			JOptionPane.showMessageDialog(null, "配置文件不完整。", "错误",
-					JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
-			return;
-		}
-		connect();
-	}
-
-	public boolean connect() {
-		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-		try {
-			Class.forName(driverName);
-			System.out.println("成功加载MySQL驱动！");
-			dbConn = DriverManager.getConnection(dbURL, userName, passwd);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-
-	public boolean isClosed() {
-		try {
-			return dbConn.isClosed();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-<<<<<<< HEAD
-    public boolean insert(String sql) {
+    public SQLConnection() {
+        JSONTokener tokener;
         try {
-            Statement stmt = dbConn.createStatement();
-            int lines = stmt.executeUpdate(sql);
-            if (lines != 0) {
+            tokener = new JSONTokener(new FileReader(configFile));
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "找不到配置文件。", "错误",
+                    JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+            return;
+        }
+        JSONObject jo = new JSONObject(tokener);
+        try {
+            dbURL = jo.getString("database_url");
+            userName = jo.getString("database_username");
+            passwd = jo.getString("database_password");
+        } catch (JSONException e) {
+            JOptionPane.showMessageDialog(null, "配置文件不完整。", "错误",
+                    JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+            return;
+        }
+        connect();
+    }
+
+    public boolean connect() {
+        String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        try {
+            Class.forName(driverName);
+            System.out.println("成功加载MySQL驱动！");
+            dbConn = DriverManager.getConnection(dbURL, userName, passwd);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isClosed() {
+        try {
+            return dbConn.isClosed();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insert(String sql, ByteArrayInputStream bais) {
+        try {
+            PreparedStatement stmt = dbConn.prepareStatement(sql);
+            stmt.setBinaryStream(1, bais);
+            int lines = stmt.executeUpdate();
+            if (lines > 0) {
                 return true;
             } else {
                 return false;
             }
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
             return false;
         }
-
     }
 
-    public static void main(String[] srg) {
-=======
-	public boolean insert(String sql, ByteArrayInputStream bais) {
-		try {
-			PreparedStatement stmt = dbConn.prepareStatement(sql);
-			stmt.setBinaryStream(1, bais);
-			int lines = stmt.executeUpdate();
-			if (lines > 0) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
->>>>>>> 684225cc7bb26d1ee97e79544279a38be5c74d5c
+    private void test() throws SQLException {
+        String sql = "insert into table_alarminfo (imageid, filecontent) values(1, ?)";
+        PreparedStatement stmt = dbConn.prepareStatement(sql);
+        stmt.setInt(1, 123);
+        stmt.setInt(2, 1234);
+    }
 
-	private void test() throws SQLException{
-		String sql = "insert into table_alarminfo (imageid, filecontent) values(1, ?)";
-		PreparedStatement stmt = dbConn.prepareStatement(sql);
-		stmt.setInt(1, 123);
-		stmt.setInt(2, 1234);
-	}
-	public static void main(String[] args) throws SQLException {
-		SQLConnection conn = new SQLConnection();
-		conn.test();
-	}
+    public static void main(String[] args) throws SQLException {
+        SQLConnection conn = new SQLConnection();
+        conn.test();
+    }
 }
---
+++ src/others/DryRun.java
@@ -0,0 +1,145 @@
+package others;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.bytedeco.javacpp.opencv_core.CvRect;
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacpp.opencv_core.Mat;
+
+import edu.bit.eline.detection.Blob;
+import edu.bit.eline.detection.BlobAnalyzer;
+import edu.bit.eline.detection.Detector;
+import edu.bit.eline.detection.DirProcesser;
+import edu.bit.eline.detection.ImageConverter;
+
+public class DryRun {
+
+    public static void run(String path, String dest) {
+        path += '/';
+        dest += '/';
+        String[] subDirs = DirProcesser.getSubDirs(path);
+
+        int subimgCount = 0;
+        for (String subDir : subDirs) {
+            String dir = path + subDir + '/';
+            String[] imgList = DirProcesser.getFilenames(dir, "jpg");
+            Detector detector = new Detector(0, 9, false);
+            BlobAnalyzer analyzer = new BlobAnalyzer(2000);
+            ImageConverter converter = new ImageConverter();
+
+            int srcImgIndex = 0;
+            for (int i = 0; i < imgList.length; ++i) {
+                String imgName = dir + imgList[i];
+                BufferedImage img;
+                try {
+                    img = ImageIO.read(new File(imgName));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                    continue;
+                }
+
+                srcImgIndex += 1;
+                Mat imgMat = converter.convert2Mat(img);
+                if (imgMat == null)
+                    continue;
+                IplImage mask = detector.detect(imgMat, 0.1, true);
+
+                if (srcImgIndex < 10)
+                    continue;
+                List<Blob> blobList;
+                blobList = analyzer.analyze(mask);
+                for (Blob blob : blobList) {
+                    CvRect rect = blob.getRect();
+                    if (!sizeLimit(rect))
+                        continue;
+                    int x = rect.x();
+                    int y = rect.y();
+                    int width = rect.width();
+                    int height = rect.height();
+                    BufferedImage subimg = img.getSubimage(x, y, width, height);
+                    try {
+                        ImageIO.write(subimg, "bmp", new File(dest + subimgCount + ".bmp"));
+                        subimgCount += 1;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        continue;
+                    }
+                    if (subimgCount % 100 == 0)
+                        System.out.println(subimgCount);
+                }
+            }
+        }
+    }
+
+    // protected CvRect getTag(String imgName) throws IOException {
+    // BufferedReader br = new BufferedReader(new FileReader(new File(imgName +
+    // ".txt")));
+    // String line;
+    // while ((line = br.readLine()) != null) {
+    // if (line.startsWith("LeftTop"))
+    // break;
+    // }
+    // String _x = line.substring(line.indexOf("(") + 1, line.indexOf(","));
+    // String _y = line.substring(line.indexOf(",") + 1, line.indexOf(")"));
+    // line = br.readLine();
+    // String _m = line.substring(line.indexOf("(") + 1, line.indexOf(","));
+    // String _n = line.substring(line.indexOf(",") + 1, line.indexOf(")"));
+    // int x = Integer.parseInt(_x);
+    // int y = Integer.parseInt(_y);
+    // int w = Integer.parseInt(_m) - x;
+    // int h = Integer.parseInt(_n) - y;
+    // Rect rect = new Rect(x, y, w, h);
+    // br.close();
+    // return rect.asCvRect();
+    // }
+    //
+    // protected boolean include(CvRect tag, CvRect rect) {
+    // int tagx1 = tag.x();
+    // int tagy1 = tag.y();
+    // int tagx2 = tagx1 + tag.width();
+    // int tagy2 = tagy1 + tag.height();
+    //
+    // int rctx1 = rect.x();
+    // int rcty1 = rect.y();
+    // int rctx2 = rctx1 + rect.width();
+    // int rcty2 = rcty1 + rect.height();
+    //
+    // if (rctx1 >= tagx1 && rctx1 <= tagx2) {
+    // if ((rcty1 >= tagy1 && rcty1 <= tagy2) || (rcty2 >= tagy1 && rcty2 <=
+    // tagy2)) {
+    // return false;
+    // }
+    // }
+    // if (rctx2 >= tagx1 && rctx2 <= tagx2) {
+    // if ((rcty1 >= tagy1 && rcty1 <= tagy2) || (rcty2 >= tagy1 && rcty2 <=
+    // tagy2)) {
+    // return false;
+    // }
+    // }
+    // return true;
+    // }
+
+    protected static boolean sizeLimit(CvRect rect) {
+        int height = rect.height();
+        int width = rect.width();
+        int size = height * width;
+        if (size < 750 || size > 50000) {
+            return false;
+        }
+        if (height < 30 || width < 30) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String path = "E:\\train\\";
+        String dest = "E:\\negative3\\";
+        run(path, dest);
+    }
+}
